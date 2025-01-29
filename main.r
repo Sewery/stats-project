94c9acd6836f0eab92f3d5428fed7dafad8ef3b6
jansarba <jansarbasarba@gmail.com>
2025-01-29 14:06:37 +0100

    refactor

--- main.r
+++ main.r
@@ -16,9 +16,9 @@ ms_algo <- function(dim, lower_b, upper_b, fun, number_of_points) {
       min_val <- ms$value
     }
   }
-  # print(min_point)
   return(c(min_val, num_of_cals))
 }
+
 prs_algo <- function(dim, lower_b, upper_b, fun, number_of_points) {
   min_val <- Inf
   for (i in 1:number_of_points) {
@@ -30,11 +30,11 @@ prs_algo <- function(dim, lower_b, upper_b, fun, number_of_points) {
   }
   return(min_val)
 }
+
 compare <- function(fun, dim) {
   lower_b <- as.numeric(getLowerBoxConstraints(fun))[1]
   upper_b <- as.numeric(getUpperBoxConstraints(fun))[1]
-
-  # Ms
+  
   print("MS:")
   res_1 <- ms_algo(dim, lower_b, upper_b, fun, 100)
   budget <- round(res_1[2] / 100)
@@ -42,8 +42,7 @@ compare <- function(fun, dim) {
   res_ms <- c(res_1[1], res_2[1, ])
   avg_min_ms <- mean(res_ms)
   print(paste("Wartość średnia:", avg_min_ms))
-
-  # Prs
+  
   print("PRS:")
   res_prs <- replicate(50, prs_algo(dim, lower_b, upper_b, fun, 50 * budget))
   avg_min_prs <- mean(res_prs)
@@ -51,28 +50,22 @@ compare <- function(fun, dim) {
   print("-------")
   return(list(res_ms, res_prs))
 }
+
+# Generate histograms
 generate_hist <- function(values, title) {
-  h <- hist(values,
-    main = title,
-    xlab = "Wartości minimum",
-    ylab = "Częstość",
-    col = "lightblue",
-    border = "black",
-    labels = TRUE
+  hist(values,
+       main = title,
+       xlab = "Wartości minimum",
+       ylab = "Częstość",
+       col = "lightblue",
+       border = "black",
+       labels = TRUE
   )
 }
-generate_hist <- function(values, title) {
-  h <- hist(values,
-    main = title,
-    xlab = "Wartości minimów",
-    ylab = "Częstość",
-    col = "lightblue",
-    border = "black",
-    labels = TRUE
-  )
-}
+
+# Generate boxplots
 generate_boxplot <- function(values_ms, values_prs, title) {
-  b <- boxplot(
+  boxplot(
     values_ms,
     values_prs,
     main = title,
@@ -80,65 +73,67 @@ generate_boxplot <- function(values_ms, values_prs, title) {
     ylab = "Wartość"
   )
 }
-## 2D for Ackley function
+
+# Wszystkie wykresy
+generate_all_plots <- function(results, function_name, dimension) {
+  # Histogramy
+  generate_hist(results[[1]], paste("Histogram dla", function_name, dimension, "D (MS)"))
+  generate_hist(results[[2]], paste("Histogram dla", function_name, dimension, "D (PRS)"))
+  
+  # Pudełkowe
+  generate_boxplot(
+    results[[1]],
+    results[[2]],
+    paste("Wykres pudełkowy dla", function_name, dimension, "D")
+  )
+}
+
+
 print("----------------------")
 print("Funkcja Ackleya:")
 print("--------------")
+
+# 2D Ackley
 print("Wartości minimalne dla 2D")
 fn <- makeAckleyFunction(2)
 results <- compare(fn, 2)
-generate_hist(results[[1]], "Wykres czestości dla funkcji Ackleya 2D dla MS")
-generate_hist(results[[2]], "Wykres czestości dla funkcji Ackleya 2D dla PS")
-generate_boxplot(
-  results[[1]],
-  results[[2]],
-  "Wykres pudelkowy dla funkcji Ackleya 2D"
-)
-#Mozesz podmienic na csc
-dir.create("./data", showWarnings = FALSE)
-write.table(results[[1]],file="./data/ms-data-ackley-2d.data")
-write.table(results[[2]],file="./data/ps-data-ackley-2d.data")
-# res_read<-read.table(file="ms-data-ackley-2d.data")
-# print(res_read)
+generate_all_plots(results, "Ackley", 2)
 
-# 10D for Ackley function
+# 10D Ackley
 print("---------")
 print("Wartości minimalne dla 10D")
 fn <- makeAckleyFunction(10)
 results <- compare(fn, 10)
-# generate_hist(results[[1]], "Wykres czestości dla funkcji Ackleya 10D dla MS")
-# generate_hist(results[[2]], "Wykres czestości dla funkcji Ackleya 10D dla PS")
-write.table(results[[1]],file="./data/ms-data-ackley-10d.data")
-write.table(results[[2]],file="./data/ps-data-ackley-10d.data")
+generate_all_plots(results, "Ackley", 10)
 
-## 20D for Ackley function
+# 20D Ackley
 print("---------")
 print("Wartości minimalne dla 20D")
 fn <- makeAckleyFunction(20)
 results <- compare(fn, 20)
-write.table(results[[1]],file="./data/ms-data-ackley-20d.data")
-write.table(results[[2]],file="./data/ps-data-ackley-20d.data")
+generate_all_plots(results, "Ackley", 20)
 
-## 2D for Schwafel function
+
 print("---------")
+print("Funkcja Schwefela:")
+print("--------------")
+
+# 2D Schwefel
 print("Wartości minimalne dla 2D")
 fn <- makeSchwefelFunction(2)
 results <- compare(fn, 2)
-write.table(results[[1]],file="./data/ms-data-schwafel-2d.data")
-write.table(results[[2]],file="./data/ps-data-schwafel-2d.data")
+generate_all_plots(results, "Schwefel", 2)
 
-## 10D for Schwafel function
+# 10D Schwefel
 print("---------")
 print("Wartości minimalne dla 10D")
 fn <- makeSchwefelFunction(10)
 results <- compare(fn, 10)
-write.table(results[[1]],file="./data/ms-data-schwafel-10d.data")
-write.table(results[[2]],file="./data/ps-data-schwafel-10d.data")
+generate_all_plots(results, "Schwefel", 10)
 
-## 20D for Schwafel function
+# 20D Schwefel
 print("---------")
 print("Wartości minimalne dla 20D")
 fn <- makeSchwefelFunction(20)
 results <- compare(fn, 20)
-write.table(results[[1]],file="./data/ms-data-schwafel-20d.data")
-write.table(results[[2]],file="./data/ps-data-schwafel-20d.data")
+generate_all_plots(results, "Schwefel", 20)
